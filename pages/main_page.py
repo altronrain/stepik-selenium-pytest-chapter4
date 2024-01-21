17e6d647d9049aea8ec64e2fa3f3b027b1ad6ec1
altronrain <altron24121992@gmail.com>
2024-01-22 02:10:43 +0300

    move login link logic to base page class

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,15 +5,5 @@ from .locators import MainPageLocators
 
 class MainPage(BasePage):
     
-    def go_to_login_page(self):
-        # Подход с неявным переходом, но явной инициализацией страницы
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        login_link.click()
-        # Подход с явным переходом, но неявной инициализацией страницы (добавить)
-        # return LoginPage(browser=self.browser, url=self.browser.current_url)
-        
-    
-    def should_be_login_link(self):
-        # self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
-        assert self.is_element_present(
-            *MainPageLocators.LOGIN_LINK), "Login link is not presented"
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
